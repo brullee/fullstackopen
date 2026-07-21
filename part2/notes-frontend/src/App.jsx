3497d25054d389ebf77bea2df25e9ca33b46c810
Abdalla Wohoush <abdalla.wohoush@gmail.com>
2026-07-21 10:33:12 +0300

    revert part2 to it's original "finished" state

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -11,9 +11,7 @@ const App = () => {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
+
 
   useEffect(() => {
     noteService
@@ -63,48 +61,6 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
-const handleLogin = async event => {
-  event.preventDefault()
-  
-  try {
-    const user = await loginService.login({ username, password })
-    setUser(user)
-    setUsername('')
-    setPassword('') 
-  } catch {
-    setErrorMessage('wrong credentials')
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 5000)
-    }
-  }
-
-  const loginForm = () => (
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>
-            username
-            <input
-              type="text"
-              value={username}
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            password
-            <input
-              type="password"
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </label>
-        </div>
-        <button type="submit">login</button>
-      </form>
-  )
-
   const noteForm = () => (
     <form onSubmit={addNote}>
       <input value={newNote} onChange={handleNoteChange} />
@@ -120,13 +76,6 @@ const handleLogin = async event => {
       <h1>Notes</h1>
       <Notification message={errorMessage} />
 
-      {!user && loginForm()}
-      {user && (
-        <div>
-            <p>{user.name} logged in</p>
-            {noteForm()}
-        </div>
-      )}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -141,7 +90,10 @@ const handleLogin = async event => {
           />
         ))}
       </ul>
-
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
       <Footer />
     </div>
   );
