b7f9c98d27ba2bbf6dc8f0e3691c7ff7475f0b9f
Abdalla Wohoush <abdalla.wohoush@gmail.com>
2026-07-29 16:02:41 +0300

    finish exercise 5.23

--- part5/Exercises/e2e/tests/bloglist.spec.js
+++ part5/Exercises/e2e/tests/bloglist.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith, createBlog } = require('./helper')
+const { loginWith, createBlog, addLike, getLikes } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -23,7 +23,7 @@ describe('Blog app', () => {
   })
 
   test('Login form is shown', async ({ page }) => {
-    await expect(page.getByLabel('username'))
+    await expect(page.getByLabel('username')).toBeVisible()
   })
 
     describe('Login', () => {
@@ -40,7 +40,7 @@ describe('Blog app', () => {
     })
   })
 
-  describe('When logged in', () => {
+  describe('When logged  in', () => {
   beforeEach(async ({ page }) => {
     await loginWith(page, 'dbuser', 'dbpassword')
   })
@@ -72,7 +72,7 @@ describe('Blog app', () => {
         await expect(page.getByText('title by author')).not.toBeVisible  ()
       })
 
-      test('a blog can be deleted by a different user', async ({ page }) => {
+      test('a blog can\'t be deleted by a different user', async ({ page }) => {
         await page.getByRole('button', { name: 'logout' }).click()
         await loginWith(page, 'dbuser2', 'dbpassword2')
 
@@ -82,6 +82,41 @@ describe('Blog app', () => {
       })
     })
 
+  describe('When many blogs exist', () => {
+    beforeEach(async ({ page }) => {
+      await createBlog(page, 'title', 'author', 'url')
+      await createBlog(page, 'title2', 'author2', 'url2')
+      await createBlog(page, 'title3', 'author3', 'url3')
+
+      await addLike(page, 'title', 'author')
+      await addLike(page, 'title', 'author')
+      await addLike(page, 'title', 'author')
+      await addLike(page, 'title', 'author')
+      await addLike(page, 'title', 'author')
+
+      await addLike(page, 'title2', 'author2')
+      await addLike(page, 'title2', 'author2')
+
+      await addLike(page, 'title3', 'author3')
+      await addLike(page, 'title3', 'author3')
+      await addLike(page, 'title3', 'author3')
+
+    })
+
+    test('blogs ordered by like count', async ({page}) => {
+      await page.getByText('title by author').getByRole('button', { name: 'view' }).click()
+      await page.getByText('title2 by author2').getByRole('button', { name: 'view' }).click()
+      await page.getByText('title3 by author3').getByRole('button', { name: 'view' }).click()
+
+      const likeArray = [
+        await getLikes(page, 'title', 'author'),
+        await getLikes(page, 'title3', 'author3'),
+        await getLikes(page, 'title2', 'author2'),
+      ]
+
+      expect(likeArray).toEqual([5,3,2])
+    })
+  })
   })
 
 })
--- part5/Exercises/e2e/tests/helper.js
+++ part5/Exercises/e2e/tests/helper.js
@@ -1,3 +1,5 @@
+const { expect } = require('@playwright/test')
+
 const loginWith = async (page, username, password)  => {
   await page.getByLabel('username').fill(username)
   await page.getByLabel('password').fill(password)
@@ -13,4 +15,22 @@ const createBlog = async (page, title, author, url) => {
   await page.getByText(`${title} by ${author}`).waitFor()
 }
 
-export { loginWith, createBlog }
+const getLikes = async (page, title, author) => {
+  const blog = page.getByText(`${title} by ${author}`)
+  const likesLocator = blog.getByText(/likes \d+/)
+  return Number((await likesLocator.textContent()).match(/likes (\d+)/)[1])
+}
+
+const addLike = async (page, title, author) => {
+  const blog = page.getByText(`${title} by ${author}`)
+  await blog.getByRole('button', { name: 'view' }).click()
+
+  const currentLikes = await getLikes(page, title, author)
+
+  await blog.getByRole('button', { name: 'like' }).click()
+  await expect(blog.getByText(/likes \d+/)).toHaveText(new RegExp(`likes ${currentLikes + 1}`))
+
+  await blog.getByRole('button', { name: 'hide' }).click()
+}
+
+export { loginWith, createBlog, addLike, getLikes }
