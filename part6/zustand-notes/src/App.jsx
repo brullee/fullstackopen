23494c3918b7b607c44cc7ed32b526d575f1c276
Abdalla Wohoush <abdalla.wohoush@gmail.com>
2026-08-02 16:19:08 +0300

    part6 init

---
+++ part6/zustand-notes/src/App.jsx
@@ -0,0 +1,13 @@
+import Display from './componenets/Display'
+import Controls from './componenets/Controls'
+
+const App = () => {
+  return (
+    <div>
+      <Display />
+      <Controls />
+    </div>
+  )
+}
+
+export default App
