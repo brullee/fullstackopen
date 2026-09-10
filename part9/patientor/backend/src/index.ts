e9604a4c9d8077bab2fcb7e4ff6388f12341744f
Abdalla Wohoush <abdalla.wohoush@gmail.com>
2026-09-10 10:56:16 +0300

    finish exercise 11

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import cors from "cors";
+import diagnosesRouter from "./routes/diagnosesRouter.ts";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor/backend/src/services/diagnosesService.ts
@@ -0,0 +1,12 @@
+import diagnosesData from "../data/diagnoses.ts";
+import type { Diagnosis } from "../types.ts";
+
+const diagnoses: Diagnosis[] = diagnosesData;
+
+const getDiagnoses = (): Diagnosis[] => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses,
+};
