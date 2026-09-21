dfbae31d9c8e6e01e618b9a31bf5ea20b4c61e60
Abdalla Wohoush <abdalla.wohoush@gmail.com>
2026-09-21 16:46:33 +0300

    finish exercise 9 part11

--- part11/pokedex/e2e-tests/example.spec.js
+++ part11/pokedex/e2e-tests/example.spec.js
@@ -2,12 +2,19 @@ import { test, describe, expect } from '@playwright/test'
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
         'Pokémon and Pokémon character names are trademarks of Nintendo.',
       ),
     ).toBeVisible()
+  })
+  test("user can navigate to ivysaur's page from main page", async ({
+    page,
+  }) => {
+    await page.goto('/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
