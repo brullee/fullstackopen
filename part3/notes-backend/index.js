8a431ef76c99b1cd6e356643201493447e8d0e5c
Abdalla Wohoush <abdalla.wohoush@gmail.com>
2026-09-22 17:51:52 +0300

    add healthcheck endpoint

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -79,6 +79,10 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
